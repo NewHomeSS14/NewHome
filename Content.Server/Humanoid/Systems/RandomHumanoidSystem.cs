b92b323dc9c1b7dd3aa439daa31b423839c62fce
Cygnall <31330700+Cygnall@users.noreply.github.com>
2025-11-18 05:01:33 -0500

    Fix random humanoids only being male

--- Content.Server/Humanoid/Systems/RandomHumanoidSystem.cs
+++ Content.Server/Humanoid/Systems/RandomHumanoidSystem.cs
@@ -44,8 +44,6 @@ public sealed class RandomHumanoidSystem : EntitySystem
 
         _metaData.SetEntityName(humanoid, prototype.RandomizeName ? profile.Name : name);
 
-        _humanoid.LoadProfile(humanoid, profile);
-
         if (prototype.Components != null)
         {
             foreach (var entry in prototype.Components.Values)
@@ -57,6 +55,9 @@ public sealed class RandomHumanoidSystem : EntitySystem
         }
 
         EntityManager.InitializeAndStartEntity(humanoid);
+
+        // Aurora Song: Load profile AFTER initialization to prevent it from being overwritten by OnInit's DefaultWithSpecies call
+        _humanoid.LoadProfile(humanoid, profile);
 
         return humanoid;
     }
